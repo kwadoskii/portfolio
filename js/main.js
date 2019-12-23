dd8fb34a5b23db9c71f54bcf7e181571cfa9428b
= <=>
2019-12-23 17:30:58 +0100

    Initial Commit

---
+++ js/main.js
@@ -0,0 +1,17 @@
+window.onscroll = function() {stickynavbar()};
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function stickynavbar() 
+{
+//    if (window.pageYOffset >= sticky) {
+//     navbar.style.display = 'none';
+//    } else {
+//     navbar.style.display = '';
+//    }
+   if (window.pageYOffset >= sticky) {
+     navbar.classList.add("sticky");
+   } else {
+     navbar.classList.remove("sticky");
+   }
+}
